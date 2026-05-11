014cefad3c30980647a17f6e1c54476d8b6e4aae
Finn Ganser <finn.ganser@gmx.de>
2026-05-11 14:32:43 +0200

    Added doxygen comments for the Core & Event class/file

--- include/Core.hpp
+++ include/Core.hpp
@@ -1,3 +1,6 @@
+//! \file Core.hpp
+//! \brief Core class where important Singleton instances are concentrated
+
 #pragma once
 
 // Project includes
@@ -11,59 +14,84 @@
 /*
  *	Public constexpr
  */
+//! \brief The CAN ID of the Master / Sensor PCB
 constexpr uint8_t MASTER_CAN_ID = 1;
 
 /*
  *	Class
  */
+//! \brief Core class as Singleton
 class Core
 {
 public:
 	/*
 	 *	Public Functions
 	 */
+	//! \brief Singleton get function
+	//! \retval The instance of the Core
 	static Core* get();
 
+	//! \brief Returns a ptr to the instance of the Display Driver
+	//! \retval Ptr to the \c ST77916 instance
 	ST77916* getDisplayDriver() const;
 
+	//! \brief Returns a ptr to the instance of the Gui
+	//! \retval Ptr to the \c Gui instance
 	Gui* getGui() const;
 
+	//! \brief Returns a ptr to the instance of the config file on the spiffs partition
+	//! \retval Ptr to the \c Config instance
 	ArduinoJson::JsonDocument* getConfig() const;
 
+	//! \brief Writes all changes of the Config file to the spiffs partition
 	void saveConfig() const;
 
 	/*
 	 *	CAN related functions
-	 */
+	*/
+	//! \brief Returns a ptr to the instance of the CAN node
+	//! \retval Ptr to the \c CAN instance
 	Can* getCan() const;
 
+	//! \brief Changes the ID used for sending & receiving on the CAN bus
+	//! \param canId The new CAN ID
 	void setCanId(const uint8_t& canId);
 
+	//! \brief Returns the current CAN ID
+	//! \retval The current CAN ID
 	uint8_t getCanId() const;
 
 private:
 	/*
 	 *	Instances
 	 */
+	//! \brief The ptr to the self instance
 	static Core* self_;
 
+	//! \brief The ptr to the CAN instance
 	Can* can_ = nullptr;
 
+	//! \brief The ptr to the ST77916 instance
 	ST77916* displayDriver_ = nullptr;
 
+	//! \brief The ptr to the Gui instance
 	Gui* gui_ = nullptr;
 
 	/*
 	 *	Private Functions
 	 */
+	//! \brief Private constructor. Instantiates & prepares everything
 	Core();
 
 	/*
 	 *	Private Variables
 	 */
+	//! \brief The current CAN ID
 	uint8_t canId_ = 0;
 
+	//! \brief Ptr to the Config instance
 	Config* config_ = nullptr;
 
+	//! \brief Ptr to the JSON representation of the Config instance
 	ArduinoJson::JsonDocument* jsonConfig_ = nullptr;
 };
--- include/Event.hpp
+++ include/Event.hpp
@@ -1,21 +1,31 @@
+//! \file Event.hpp
+//! \brief Declaration of an Event used in internal FreeRTOS Queues
+
 #pragma once
 
+//! \brief Represents an Event
 struct Event
 {
+	//! \brief The type of the event
+	//!
+	//! Contains all types of events: For the GUI, for the StateMachine etc.
 	typedef enum
 	{
-		UNKNOWN,
-		SET_SCREEN,
-		WAKE_UP,
+		UNKNOWN,		//!< Unknown Event, used for error detection
+		SET_SCREEN,		//!< Loads the specified screen in the GUI
+		WAKE_UP,		//!< Turns on the BL of the ST77916 and displays the GUI
 	} TYPE;
 
+	//! \brief Default constructor of the Event
 	Event(const TYPE type = UNKNOWN, const int data = 0)
 	{
 		this->type = type;
 		this->data = data;
 	}
 
+	//! \brief Public instance of the TYPE enum
 	TYPE type = UNKNOWN;
 
+	//! \brief Public Integer data
 	int data = 0;
 };
